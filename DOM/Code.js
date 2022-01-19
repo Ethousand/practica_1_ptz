c6b88ca3775b5b16a760343c20e9aa5ded80797a
Emil Colina <cybre333@gmail.com>
2022-01-19 16:38:49 -0400

    Convirtiendo codigo suelto en funciones

--- DOM/Code.js
+++ DOM/Code.js
@@ -2,15 +2,15 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 console.log(lienzo);
 
-lienzo.beginPath();
-lienzo.strokeStyle = "darkgreen"
-lienzo.moveTo(100, 100);
-lienzo.lineTo(200, 200);
-lienzo.stroke();
-lienzo.closePath();
+Drawing_lines(10, 300, 220, 10, "pink");
+Drawing_lines(300, 10, 10, 220, "yellow");
 
-
-function Drawing_lines()
+function Drawing_lines(Xi, Yi, Xf, Yf, color)
 {
-    
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(Xi, Yi);
+    lienzo.lineTo(Xf, Yf);
+    lienzo.stroke();
+    lienzo.closePath();
 }
