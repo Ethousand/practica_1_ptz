bb7042dbff43666711f83f6b58fc1e08ca883872
Emil Colina <cybre333@gmail.com>
2022-01-24 13:28:19 -0400

    Uso de ciclos para el dibujado en Dom
    uso de ciclos para el dibujado en Dom

--- DOM/Code.js
+++ DOM/Code.js
@@ -1,9 +1,64 @@
+
+
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-console.log(lienzo);
+var lineas, l, Xinicial, Yinicial, Xfinal, Yfinal, Fcolor;
+lineas = 30;
 
-Drawing_lines(10, 300, 220, 10, "pink");
-Drawing_lines(300, 10, 10, 220, "yellow");
+// from up-left to down-right
+for (l= 0; l < lineas; l++)
+{
+    Xinicial = 0;
+    Xfinal = (l+1) * 10;
+    
+    Yinicial = l*10;
+    Yfinal = 300;
+
+    Fcolor = "#AAF";
+    Drawing_lines(Xinicial, Xfinal, Yinicial, Yfinal, Fcolor);
+    
+}
+
+//from up-left to right-down
+for (l = 0; l < lineas; l++)
+{
+    Xinicial = l*10;
+    Xfinal = 300; 
+
+    Yfinal = (l+1)*10;
+    Yinicial = 0;
+    Fcolor = "#03C530";
+
+    Drawing_lines(Xinicial, Yinicial, Xfinal, Yfinal, Fcolor);
+
+}
+
+//from up-right to left-down
+for (l = 0; l < lineas ; l++)
+{
+    Xinicial = l*10;
+    Xfinal = 300;
+
+    Yinicial = 300;
+    Yfinal = 300 - ((l+1)*10)
+    Fcolor = "#ffa500";
+
+    Drawing_lines(Xinicial, Yinicial, Xfinal, Yfinal, Fcolor);
+}
+
+//from down-left to up-right
+for (l = 0; l < lineas; l++)
+{
+    Xinicial = 0;
+    Xfinal = (l+1)*10;
+
+    Yinicial = 300 - (l*10);
+    Yfinal = 0;
+    Fcolor = "grey";
+
+    Drawing_lines(Xinicial, Yinicial, Xfinal, Yfinal, Fcolor);   
+}
+
 
 function Drawing_lines(Xi, Yi, Xf, Yf, color)
 {
