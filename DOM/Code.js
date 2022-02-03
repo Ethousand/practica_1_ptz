93997d826d929030707c9b78cc91a0faf0e9550d
Emil Colina <cybre333@gmail.com>
2022-02-03 02:06:00 -0400

    Extrayendo informacion de formularios de html, uso de detector de eventos para disparar Funciones

--- DOM/Code.js
+++ DOM/Code.js
@@ -1,63 +1,18 @@
 
 
 var d = document.getElementById("dibujito");
+ancho = d.width, alto = d.clientHeight
+
+
 var lienzo = d.getContext("2d");
-var lineas, l, Xinicial, Yinicial, Xfinal, Yfinal, Fcolor;
-lineas = 30;
+var lineas, l = 0, Xinicial, Yinicial, Xfinal, Yfinal, Fcolor;
+
+var texto = document.getElementById("input_box");
+
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", Dibujuro_por_click);
 
 // from up-left to down-right
-for (l= 0; l < lineas; l++)
-{
-    Xinicial = 0;
-    Xfinal = (l+1) * 10;
-    
-    Yinicial = l*10;
-    Yfinal = 300;
-
-    Fcolor = "#AAF";
-    Drawing_lines(Xinicial, Xfinal, Yinicial, Yfinal, Fcolor);
-    
-}
-
-//from up-left to right-down
-for (l = 0; l < lineas; l++)
-{
-    Xinicial = l*10;
-    Xfinal = 300; 
-
-    Yfinal = (l+1)*10;
-    Yinicial = 0;
-    Fcolor = "#03C530";
-
-    Drawing_lines(Xinicial, Yinicial, Xfinal, Yfinal, Fcolor);
-
-}
-
-//from up-right to left-down
-for (l = 0; l < lineas ; l++)
-{
-    Xinicial = l*10;
-    Xfinal = 300;
-
-    Yinicial = 300;
-    Yfinal = 300 - ((l+1)*10)
-    Fcolor = "#ffa500";
-
-    Drawing_lines(Xinicial, Yinicial, Xfinal, Yfinal, Fcolor);
-}
-
-//from down-left to up-right
-for (l = 0; l < lineas; l++)
-{
-    Xinicial = 0;
-    Xfinal = (l+1)*10;
-
-    Yinicial = 300 - (l*10);
-    Yfinal = 0;
-    Fcolor = "grey";
-
-    Drawing_lines(Xinicial, Yinicial, Xfinal, Yfinal, Fcolor);   
-}
 
 
 function Drawing_lines(Xi, Yi, Xf, Yf, color)
@@ -68,4 +23,58 @@ function Drawing_lines(Xi, Yi, Xf, Yf, color)
     lienzo.lineTo(Xf, Yf);
     lienzo.stroke();
     lienzo.closePath();
+}
+
+function Dibujuro_por_click()
+{
+    lineas = parseInt(texto.value);
+    var espacio = ancho / lineas
+
+    while (l < lineas)
+{
+    Xinicial = 0;
+    Xfinal = (l+1) * espacio;
+    
+    Yinicial = l* espacio;
+    Yfinal = 300;
+ 
+    Fcolor = "#AAF";
+    Drawing_lines(Xinicial, Xfinal, Yinicial, Yfinal, Fcolor);
+    l++;
+    }
+
+//from up-left to right-down
+    for (l = 0; l < lineas; l++)
+    {
+        Xinicial = l*espacio;
+        Xfinal = ancho; 
+
+        Yfinal = (l+1)*espacio;
+        Yinicial = 0;
+        Fcolor = "#03C530";
+
+        Drawing_lines(Xinicial, Yinicial, Xfinal, Yfinal, Fcolor);
+
+        //from up-right to left-down
+        Xinicial = l*espacio;
+        Xfinal = ancho;
+
+        Yinicial = alto;
+        Yfinal = alto - ((l+1)*espacio)
+        Fcolor = "#ffa500";
+
+        Drawing_lines(Xinicial, Yinicial, Xfinal, Yfinal, Fcolor);
+
+        //from down-left to up-right
+        Xinicial = 0;
+        Xfinal = (l+1)*espacio;
+
+        Yinicial = ancho - (l*espacio);
+        Yfinal = 0;
+        Fcolor = "grey";
+
+        Drawing_lines(Xinicial, Yinicial, Xfinal, Yfinal, Fcolor);
+
+    }
+    
 }
